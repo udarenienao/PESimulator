963ef18cb3e8142f5859de9b27b1b1824713650f
udarenienao <72667318+udarenienao@users.noreply.github.com>
2022-05-19 23:15:11 +0500

    fix forms

--- scripts.js
+++ scripts.js
@@ -4,25 +4,21 @@ for(let i = 1; i < 7; i++){
     let form = document.getElementById("f" + i);
     form.addEventListener("click", function (){
         subMaker.style.visibility = "visible";
-        subMaker.addEventListener("submit", function (event) {
+        subMaker.addEventListener("submit", function handler(event) {
             event.preventDefault();
-            retrieveFromValue(form, i);
+            let name = subMaker.querySelector('[name="name"]').value;
+            let teacher = subMaker.querySelector('[name="select"] option:checked').textContent;
+            let diff = subMaker.querySelector('[name="difficulty"]').value;
+            let hours = subMaker.querySelector('[name="hours"]').value;
+            subjects[i] = new Subject(name, teacher, diff, hours);
+            let newForm = document.getElementById("f" + i + i);
+            form.style.visibility = "hidden";
+            newForm.style.visibility = "visible";
+            newForm.appendChild(document.createTextNode(name))
+            subMaker.style.visibility = "hidden";
+            subMaker.removeEventListener("submit", handler)
         });
     });
-}
-
-function retrieveFromValue(form, from){
-
-    let name = subMaker.querySelector('[name="name"]').value;
-    let teacher = subMaker.querySelector('[name="select"] option:checked').textContent;
-    let diff = subMaker.querySelector('[name="difficulty"]').value;
-    let hours = subMaker.querySelector('[name="hours"]').value;
-    subjects[from] = new Subject(name, teacher, diff, hours);
-    let newForm = document.getElementById("f" + from + from);
-    form.style.visibility = "hidden";
-    newForm.style.visibility = "visible";
-    newForm.appendChild(document.createTextNode(name))
-    subMaker.style.visibility = "hidden";
 }
 
 class Subject{

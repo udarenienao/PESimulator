a047c28e7e3e8248af077d993fc29d969b8c8410
udarenienao <72667318+udarenienao@users.noreply.github.com>
2022-06-02 23:08:28 +0500

    Сережа почини(

--- scripts.js
+++ scripts.js
@@ -2,6 +2,18 @@ let subMaker = document.getElementById("makeSub");
 let subUpdater = document.getElementById("updateSub");
 let subjects = [];
 let moneyDisplay = document.getElementById("money");
+let canvas = document.getElementById('c1');
+let ctx = canvas.getContext('2d');
+let mas = [];
+let count = 0;
+let timer;
+let fieldWidth = 15;
+let fieldHeight = 15;
+let cellWidth = 450 / fieldWidth; // 450 - размер канваса
+let cellHeight = 450 / fieldHeight;
+let moneyCount = 5;
+let livesCount = 5;
+let gameStart = false;
 
 setInterval(function() {
     // console.log(subjects);
@@ -63,6 +75,102 @@ for(let i = 1; i < 7; i++){
     });
 }
 
+document.getElementById("earnMoney").addEventListener("click", function (){
+    let life = document.getElementById("life");
+    life.style.visibility = "visible";
+    goLife();
+});
+
+canvas.addEventListener("click", function(event){
+    if (gameStart && livesCount !== 0) {
+        let x = Math.floor(event.offsetY / cellHeight);
+        let y = Math.floor(event.offsetX / cellWidth);
+        if (mas[x][y] === 0) {
+            mas[x][y] = 1;
+            livesCount--;
+            drawField();
+        }
+    }
+});
+
+document.getElementById('startLife').addEventListener("click", function(evt) {
+    evt.preventDefault();
+    startLife();
+
+});
+document.getElementById('getCoins').addEventListener("click", function(evt) {
+    evt.preventDefault();
+    gameStart = true;
+});
+
+function goLife() {
+    for (let i = 0; i < fieldHeight; i++) {
+        mas[i] = [];
+        for (let j = 0; j < fieldWidth; j++) {
+            mas[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < moneyCount; i++) {
+        let x = Math.floor(Math.random() * fieldWidth);
+        let y = Math.floor(Math.random() * fieldHeight);
+        while (mas[x][y] === 2) {
+            x = Math.floor(Math.random() * fieldWidth);
+            y = Math.floor(Math.random() * fieldHeight);
+        }
+        mas[x][y] = 2;
+    }
+    drawField();
+}
+
+function drawField(){
+    ctx.clearRect(0, 0, cellWidth * fieldWidth, cellHeight * fieldHeight);
+    for (let i = 0; i < fieldHeight; i++) {
+        for (let j = 0; j < fieldWidth; j++) {
+            ctx.fillRect(i, j * cellHeight, cellWidth * fieldWidth, 1);
+            ctx.fillRect(i * cellWidth, j, 1, cellHeight * fieldHeight);
+        }
+    }
+    for (let i = 0; i < fieldHeight; i++) {
+        for (let j = 0; j < fieldWidth; j++) {
+            if (mas[i][j] === 2) {
+                ctx.drawImage(document.getElementById('c'), j * cellWidth + 1,
+                    i * cellHeight + 1, cellWidth - 1, cellHeight - 1);
+            } else if (mas[i][j] === 1) {
+                ctx.fillRect(j * cellHeight, i * cellWidth, cellWidth, cellHeight);
+            }
+        }
+    }
+}
+
+function startLife(){
+    let mas2 = [];
+    for (let i = 0; i < fieldHeight; i++){
+        mas2[i] = [];
+        for (let j = 0; j < fieldWidth; j++){
+            console.log(mas);
+            let neighbors = mas[fpm(i) - 1][j] % 2 + mas[i][fpp(j) + 1] % 2 + mas[fpp(i) + 1][j] % 2 +
+                mas[i][fpm(j) - 1] % 2 + mas[fpm(i) - 1][fpp(j) + 1] % 2 + mas[fpp(i) + 1][fpp(j) + 1] % 2 +
+                mas[fpp(i) + 1][fpm(j) - 1] % 2 + mas[fpm(i) - 1][fpm(j) - 1] % 2;
+            if (mas[i][j] === 1) {
+                mas2[i][j] = (neighbors === 2 || neighbors === 3) ? 1 : 0;
+            } else {
+                mas2[i][j] = neighbors === 3 ? 1 : 0;
+                if (mas2[i][j] === 1) {
+                    if (mas[i][j] === 2) {
+                        //тут должно быть прибавление монетки в игре
+                    }
+                } else if (mas[i][j] === 2) {
+                    mas2[i][j] = 2;
+                }
+            }
+        }
+    }
+    mas = mas2;
+    drawField();
+    count++;
+    document.getElementById('count').textContent = count;
+    timer = setTimeout(startLife, 300);
+}
 
 class Subject{
     constructor(name, teacher, diff, hours) {
